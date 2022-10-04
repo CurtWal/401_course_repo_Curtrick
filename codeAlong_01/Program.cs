2871ccf33559e91207d6e35978934ecf24746196
Curtrick Walton <curtrickw@yahoo.com>
2022-10-04 15:11:49 +0000

    code along

---
+++ codeAlong_01/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CodeAlong_01
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            //  I want to print to the console my name and age
+            //  and a brief description of myself
+
+            string myName = " Marcus";
+
+            int marcusAge = 25;
+
+            string marcusJob = " softwareDeveloper ";
+
+            string funFact = "Marcus absolutely loves swimming!";
+
+            Console.WriteLine($"my name is {myName}, I am {marcusAge} years old. For a living I am a {marcusJob}. Lastly a fun fact: {funFact}");
+
+
+
+        }
+    }
+}
---
+++ codeAlong_01/main.cs
@@ -0,0 +1,19 @@
+//  what do we know about C-Sharp
+    // We still have VALUE-TYPES
+    // strings, integers, arrays, floats, booleans
+
+
+//type   name  assignment     value
+// int biggestNumberOnEarth = 0;
+
+//  let biggestNumberOnEarth =0;
+
+// boolean
+//  bool isComplete = false;
+
+// //  how do I make a string
+// string myGuitar = "ghost";
+
+// Array
+// int[] ages = {21,22,23,24,25,26,27,28,29,30,31};
+// Console.WriteLine(ages[3]);
